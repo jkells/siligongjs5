ee8140c0d8332fee8b202fa6f67d0c709e4dca94
Jared Kells <jared@easyagile.com>
2019-06-05 18:36:09 +1000

    Copy buttons on code snippets

--- src/components/SlideBabel.jsx
+++ src/components/SlideBabel.jsx
@@ -1,8 +1,20 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import React from "react";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const babelCode = `yarn add -D @babel/preset-typescript`;
+const babelCode = `yarn add -D \\
+    typescript \\
+    @babel/preset-typescript \\
+    @types/faker \\
+    @types/lodash \\
+    @types/react \\
+    @types/react-dom \\
+    @types/react-redux \\
+    @types/react-syntax-highlighter
+`;
+
+const typesCode = `cp tsconfig.json.sample tsconfig.json`;
 
 const renameCode = `find src -name "*.jsx" -exec rename 's/\.jsx$/.tsx/' '{}' \\;
 find src -name "*.js" -exec rename 's/\.js$/.ts/' '{}' \\;
@@ -13,15 +25,31 @@ test: /\.(js|jsx|ts|tsx)$/
 extensions: ['.js', '.jsx', '.ts', '.tsx']
 `;
 
+function CodeBlock({ language, code }) {
+    // Hacks
+    return (
+        <div style={{position: "relative"}}>
+            <CopyToClipboard text={typesCode}>
+                <button style={{position: "absolute", right: 0}}>Copy</button>
+            </CopyToClipboard>
+            <SyntaxHighlighter language={language} style={dark}>
+                {code}
+            </SyntaxHighlighter>
+        </div>
+    );
+}
+
 export default function SlideBabel() {
     return (
         <>
             <h1>Babel Project</h1>
             <div>
+                <h2>Create a .tsconfig</h2>
+                <CodeBlock languge="bash" code={typesCode} />
+            </div>
+            <div>
                 <h2>Install Typescript preset and update .babelrc</h2>
-                <SyntaxHighlighter language="bash" style={dark}>
-                    {babelCode}
-                </SyntaxHighlighter>
+                <CodeBlock language="bash" code={babelCode} />
             </div>
             <div>
                 <h2>
@@ -31,18 +59,11 @@ export default function SlideBabel() {
                     </a>
                     )
                 </h2>
-                <SyntaxHighlighter language="bash" style={dark}>
-                    {renameCode}
-                </SyntaxHighlighter>
+                <CodeBlock language="bash" code={renameCode} />
             </div>
             <div>
                 <h2>Update extensions in webpack.conf.js</h2>
-                <SyntaxHighlighter language="javascript" style={dark}>
-                    {webpackCode}
-                </SyntaxHighlighter>
-            </div>
-            <div>
-                <h2>Create a .tsconfig</h2>
+                <CodeBlock language="javascript" code={webpackCode} />
             </div>
         </>
     );
--- types.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-yarn add -D @types/faker \
-@types/lodash \
-@types/react \
-@types/react-dom \
-@types/react-redux \
-@types/react-syntax-highlighter
